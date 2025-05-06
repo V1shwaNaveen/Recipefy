29bb03e68c2b275ff6c89ee239a18c7ab52d3a19
Vishwa Warnakulasuriya <vishwanaveen.personal@gmail.com>
2025-05-06 16:12:48 +0530

    Created the backend for the Recipefy app

---
+++ recipefy-backend/src/main/java/com/recipefy/recipefy_backend/RecipefyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recipefy.recipefy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipefyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipefyBackendApplication.class, args);
+	}
+
+}
